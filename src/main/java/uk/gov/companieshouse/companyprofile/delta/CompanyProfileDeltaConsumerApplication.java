22a5091c609dee237a7240312df0d5ef0d8098d9
JAndrewCH <jandrew@companieshouse.gov.uk>
2023-06-01 14:37:20 +0100

    add pom and main

---
+++ src/main/java/uk/gov/companieshouse/companyprofile/delta/CompanyProfileDeltaConsumerApplication.java
@@ -0,0 +1,7 @@
+package uk.gov.companieshouse.companyprofile.delta;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyProfileDeltaConsumerApplication {
+}
