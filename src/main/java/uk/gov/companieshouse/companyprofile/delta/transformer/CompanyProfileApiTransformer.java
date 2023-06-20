36ad334eaf495a8100991b6c4f3d9943bcdd418d
maddytjCH <mtjalma@companieshouse.gov.uk>
2023-06-20 16:27:28 +0100

    initial commit

---
+++ src/main/java/uk/gov/companieshouse/companyprofile/delta/transformer/CompanyProfileApiTransformer.java
@@ -0,0 +1,38 @@
+package uk.gov.companieshouse.companyprofile.delta.transformer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import consumer.exception.NonRetryableErrorException;
+import uk.gov.companieshouse.api.company.CompanyProfile;
+import uk.gov.companieshouse.api.delta.CompanyDelta;
+import uk.gov.companieshouse.companyprofile.delta.mapper.CompanyProfileMapper;
+import uk.gov.companieshouse.companyprofile.delta.mapper.MapperUtils;
+
+@Component
+public class CompanyProfileApiTransformer {
+    private final CompanyProfileMapper mapper;
+
+    @Autowired
+    private MapperUtils mapperUtils;
+
+    /**
+     * Constructor for the transformer.
+     * @param mapper returns the company profile api object.
+     */
+    @Autowired
+    public CompanyProfileApiTransformer(CompanyProfileMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public CompanyProfile transform(CompanyDelta companyDelta) {
+        try {
+            CompanyProfile companyProfile = mapper.companyProfileDeltaToCompanyProfile(companyDelta);
+
+            return companyProfile;
+        } catch (Exception exception) {
+            throw new NonRetryableErrorException(exception);
+        }
+    }
+    
+}
