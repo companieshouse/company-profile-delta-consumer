a9da39146dc0de1e3856b759e2bac901ae1bb168
YaaseenR <mrahman1@companieshouse.gov.uk>
2023-07-25 14:41:42 +0100

    updated pom & reverted changes

--- src/main/java/uk/gov/companieshouse/companyprofile/delta/mapper/CompanyProfileMapper.java
+++ src/main/java/uk/gov/companieshouse/companyprofile/delta/mapper/CompanyProfileMapper.java
@@ -214,6 +214,19 @@ public abstract class CompanyProfileMapper {
         target.setData(data);
     }
 
+    /**Map isComunnityInterestCompany to string. */
+    @AfterMapping
+    public void mapCicInd(@MappingTarget CompanyProfile target, CompanyDelta source) {
+        Data data = target.getData();
+        BooleanFlag cicIndFlag = source.getCicInd();
+        String cicIndType = null;
+        if (cicIndFlag != null) {
+            cicIndType = cicIndFlag.getValue();
+        }
+        HashMap<String, Boolean> cicIndMap = MapperUtils.getIsCommunityInterestCompanyMap();
+        data.setIsCommunityInterestCompany(cicIndMap.getOrDefault(cicIndType, null));
+        target.setData(data);
+    }
 
     /**Maps enum account_type to string. */
     @AfterMapping
@@ -239,15 +252,11 @@ public abstract class CompanyProfileMapper {
     public void mapEnums(@MappingTarget CompanyProfile target, CompanyDelta source) {
         Data data = target.getData();
         if (data == null) {
-            data = new Data() ;
+            data = new Data();
         }
-        BooleanFlag subTypeFlag = source.getSubtype();
-        String subType = null;
-        if (subTypeFlag != null) {
-            subType = subTypeFlag.getValue();
-        }
+        String subTypeFlag = source.getSubtype();
         HashMap<String,String> subTypeMap = MapperUtils.getSubTypeMap();
-        data.setSubtype(subTypeMap.getOrDefault(subType,null));
+        data.setSubtype(subTypeMap.getOrDefault(subTypeFlag,null));
         target.setData(data);
     }
 
