85a7dceb79e1db6074fe053524ce8b335ba67b86
mturner-CH <mturner2@companieshouse.gov.uk>
2025-02-27 15:28:57 +0000

    DSND-2868: Remove unused import

--- src/test/java/uk/gov/companieshouse/companyprofile/delta/transformer/CompanyProfileDeltaDeserialiserTest.java
+++ src/test/java/uk/gov/companieshouse/companyprofile/delta/transformer/CompanyProfileDeltaDeserialiserTest.java
@@ -10,7 +10,6 @@ import static org.mockito.Mockito.when;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import consumer.exception.NonRetryableErrorException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
