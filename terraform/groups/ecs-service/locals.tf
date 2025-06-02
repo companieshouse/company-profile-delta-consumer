fc2ffb61af249a113b7105e0598d9e019b3372b5
vaibhavcompanieshouse <vkumar@companieshouse.gov.uk>
2025-06-02 17:18:18 +0100

    Java Upgrade
    This pr is to align base path for health check resolves https://companieshouse.atlassian.net/browse/JU-1705

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -7,7 +7,7 @@ locals {
   container_port              = "8081"
   docker_repo                 = "company-profile-delta-consumer"
   kms_alias                   = "alias/${var.aws_profile}/environment-services-kms"
-  healthcheck_path            = "/healthcheck"
+  healthcheck_path            = "/company-profile-delta-consumer/healthcheck"
   healthcheck_matcher         = "200"
   vpc_name                    = local.stack_secrets["vpc_name"]
   s3_config_bucket            = data.vault_generic_secret.shared_s3.data["config_bucket_name"]
