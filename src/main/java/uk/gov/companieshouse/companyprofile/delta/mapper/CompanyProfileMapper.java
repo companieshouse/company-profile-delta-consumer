91225e1e244bfebbd758eb66e9baeb7c13b8d2d7
mwestacott <mwestacott2@companieshouse.gov.uk>
2025-09-01 16:10:13 +0100

    Library updates to address newly reported dependency vulnerabilities
    * Some CH library versions updated
    * Some transitive library versions also updated (fixed)
    * A new exclusion added
    * Java import updated based on the newly imported class

--- src/main/java/uk/gov/companieshouse/companyprofile/delta/mapper/CompanyProfileMapper.java
+++ src/main/java/uk/gov/companieshouse/companyprofile/delta/mapper/CompanyProfileMapper.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
