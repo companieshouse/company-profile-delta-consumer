2a1b51038e60df2203d747fcc631f27e85ca09ec
mnycz-ch <167440954+mnycz-ch@users.noreply.github.com>
2025-09-05 16:40:28 +0100

    roe-add-corporate-annotations

--- src/main/java/uk/gov/companieshouse/companyprofile/delta/mapper/MapperUtils.java
+++ src/main/java/uk/gov/companieshouse/companyprofile/delta/mapper/MapperUtils.java
@@ -159,6 +159,9 @@ public class MapperUtils {
         corpAnnotationTypeMap.put("100", "other");
         corpAnnotationTypeMap.put("20", "strike-off-under-section-1002a-of-the-companies-act-2006-incorporation");
         corpAnnotationTypeMap.put("21", "strike-off-under-section-1002a-of-the-companies-act-2006-restoration");
+        corpAnnotationTypeMap.put("22", "oe-dissolved-wound-up-or-ceased-to-exist");
+        corpAnnotationTypeMap.put("23", "oe-not-complied-with-companies-house-act");
+        corpAnnotationTypeMap.put("24", "oe-failed-to-provide-sufficient-evidence");
         return corpAnnotationTypeMap;
     }
 
