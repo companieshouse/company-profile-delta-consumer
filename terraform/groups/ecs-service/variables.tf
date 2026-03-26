a608d18d8e3f889a15284d8aa9e353d1bf46aedd
tombradley1 <tbradley@companieshouse.gov.uk>
2026-03-26 09:11:28 +0000

    cidev/staging/live Increase CPU, memory and scale out values

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -83,6 +83,12 @@ variable "service_autoscale_target_value_cpu" {
   default     = 50 # 100 disables autoscaling using CPU as a metric
 }
 
+variable "service_autoscale_scale_out_cooldown" {
+  type        = number
+  description = "Cooldown in seconds for ECS Service scale out (add more tasks)"
+  default     = 300
+}
+
 variable "service_scaledown_schedule" {
   type        = string
   description = "The schedule to use when scaling down the number of tasks to zero."
